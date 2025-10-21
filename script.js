7735036b2c5461e9ea4cea9bf1cec298105a73c6
adertos <aderto2025@gmail.com>
2025-10-21 07:34:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function loadSelectedSet() {
         words = allSets[selectedDate];
         currentSet = selectedDate;
         alert(`Załadowano zestaw z dnia ${selectedDate}`);
-        document.getElementById('status').textContent = 'Click "Start Quiz",to begin ';
+        document.getElementById('status').textContent = 'Kliknij "Start Quiz",aby zacząć ';
     } else {
         words = [];
         currentSet = '';
